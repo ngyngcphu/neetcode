3aa1befdc3232038a86e273e01877658281d21db
Phu Nguyen Ngoc <111476687+ngyngcphu@users.noreply.github.com>
2026-04-18 23:46:58 +0700

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.go
@@ -0,0 +1,18 @@
+func maxProfit(prices []int) int {
+	maxPrice := -100
+	start := 0
+	for end := 1; end < len(prices); end++ {
+		diff := prices[end] - prices[start]
+		for diff < 0 && start < end {
+			start++
+		}
+		if maxPrice < prices[end] - prices[start] {
+			maxPrice = prices[end] - prices[start]
+		}
+	}
+	if maxPrice == -100 {
+		return 0
+	} else {
+		return maxPrice
+	}
+}
