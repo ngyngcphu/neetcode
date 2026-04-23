49be059ffaa01cd8af898c82e9600d98c0e8e308
Phu Nguyen Ngoc <111476687+ngyngcphu@users.noreply.github.com>
2026-04-23 23:55:04 +0700

    Add: buildings-with-an-ocean-view - submission-0

---
+++ Data Structures & Algorithms/buildings-with-an-ocean-view/submission-0.go
@@ -0,0 +1,11 @@
+func findBuildings(heights []int) []int {
+	stack := []int{}
+
+	for i, height := range heights {
+		for len(stack) > 0 && height >= heights[stack[len(stack) - 1]] {
+			stack = stack[:len(stack) - 1]
+		}
+		stack = append(stack, i)
+	}
+	return stack
+}
