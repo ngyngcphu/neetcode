4c056cc8759b105eefdac4c1140940c80eaadbdc
Phu Nguyen Ngoc <111476687+ngyngcphu@users.noreply.github.com>
2026-04-30 22:04:06 +0700

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.go
@@ -0,0 +1,14 @@
+func searchMatrix(matrix [][]int, target int) bool {
+	row := len(matrix) - 1
+	col := 0
+	for row >= 0 && col < len(matrix[0]) {
+		if matrix[row][col] == target {
+			return true
+		} else if matrix[row][col] < target {
+			col++
+		} else {
+			row--
+		}
+	}
+	return false
+}
