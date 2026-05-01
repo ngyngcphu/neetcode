fab3ef4e46555d89ae162535b1a5d65f90bc7991
Phu Nguyen Ngoc <111476687+ngyngcphu@users.noreply.github.com>
2026-05-01 16:25:39 +0700

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.go
@@ -0,0 +1,13 @@
+func uniquePaths(m int, n int) int {
+	dp := make([]int, n)
+	for i := range n {
+		dp[i] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[j] += dp[j - 1]
+		}
+	}
+	return dp[n - 1]
+}
