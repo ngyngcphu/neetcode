8c1e082dfecb18e5392c1424049ec031aecd9aa7
Phu Nguyen Ngoc <111476687+ngyngcphu@users.noreply.github.com>
2026-05-04 22:22:16 +0700

    Add: guess-number-higher-or-lower - submission-0

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-0.go
@@ -0,0 +1,24 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+func guessNumber(n int) int {
+	left := 1
+	right := n
+	for left <= right {
+		mid := (left + right) / 2
+		if guess(mid) == 0 {
+			return mid
+		} else if guess(mid) == -1 {
+			right = mid - 1
+		} else if guess(mid) == 1 {
+			left = mid + 1
+		}
+	}
+	return -1
+}
