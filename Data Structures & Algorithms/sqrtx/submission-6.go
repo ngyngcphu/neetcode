ff715a0fd328087b5fffb45f4f9deff96c050431
Phu Nguyen Ngoc <111476687+ngyngcphu@users.noreply.github.com>
2026-05-05 23:51:10 +0700

    Add: sqrtx - submission-6

---
+++ Data Structures & Algorithms/sqrtx/submission-6.go
@@ -0,0 +1,16 @@
+func mySqrt(x int) int {
+	left := 0
+	right := x
+	for left <= right {
+		mid := (left + right) / 2
+		temp := mid * mid
+		if temp == x {
+			return mid
+		} else if temp < x {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return right
+}
