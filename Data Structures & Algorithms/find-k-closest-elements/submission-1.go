4774720067623f29b2d950218634f616caf6dcb4
Phu Nguyen Ngoc <111476687+ngyngcphu@users.noreply.github.com>
2026-05-27 13:50:04 +0700

    Add: find-k-closest-elements - submission-1

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-1.go
@@ -0,0 +1,13 @@
+func findClosestElements(arr []int, k int, x int) []int {
+	left := 0
+	right := len(arr) - k
+	for left < right {
+		mid := (left + right) / 2
+		if x - arr[mid] > arr[mid + k] - x {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return arr[left:left+k]
+}
